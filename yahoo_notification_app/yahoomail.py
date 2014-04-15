ec0aa5795ac6dfe32827cd435393a148c1202e70
Shalini Roy <shaliniroy012@gmail.com>
2014-04-15 23:36:19 +0530

    rewrite the code

--- yahoo_notification_app/yahoomail.py
+++ yahoo_notification_app/yahoomail.py
@@ -1,12 +1,12 @@
-import poplib
-from email import parser
-
-pop_conn = poplib.POP3_SSL('pop.mail.yahoo.in')
-a = raw_input("Enter your yahoo email address: ")
-pop_conn.user(a)
-b = raw_input("Enter your Pssword: ")
-pop_conn.pass_(b)
-
-iMessageCount = len(pop_conn.list()[1])
-print iMessageCount
+import cookielib, urllib2
+import urllib
+LoginUrl="https://login.yahoo.com/config/login?"
+ExportUrl="http://mail.yahoo.com/"
+form_data = {'login':'example@yahoo.com/in', 'passwd':'your password'}
+form_data = urllib.urlencode(form_data)
+jar = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
+resp = opener.open(LoginUrl, form_data)
+resp = opener.open(ExportUrl)
+page = resp.read()
 
