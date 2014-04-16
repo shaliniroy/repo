490103a72703ed3df51145d4d5a9cd3e70dce3e7
Shalini Roy <shaliniroy012@gmail.com>
2014-04-16 21:45:38 +0530

    code that count number of unread yahoo mails

--- yahoo_notification_app/yahoomail.py
+++ yahoo_notification_app/yahoomail.py
@@ -1,7 +1,9 @@
 import cookielib, urllib2
 import urllib
+from bs4 import BeautifulSoup
+
 LoginUrl="https://login.yahoo.com/config/login?"
-ExportUrl="http://mail.yahoo.com/"
+ExportUrl="https://in-mg61.mail.yahoo.com/neo/b/launch?"
 form_data = {'login':'example@yahoo.com/in', 'passwd':'your password'}
 form_data = urllib.urlencode(form_data)
 jar = cookielib.CookieJar()
@@ -9,4 +11,11 @@ opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
 resp = opener.open(LoginUrl, form_data)
 resp = opener.open(ExportUrl)
 page = resp.read()
+soup = BeautifulSoup(page)
+n = soup.em.b.string
+n = str(n)
+n = n.split(')')[0].lstrip('(')
+n = int(n)
+print n
+
 
