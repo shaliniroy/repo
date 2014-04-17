0ec28e9251f2ff03ad61111f2eb7aecf09ff2ed4
Shalini Roy <shaliniroy012@gmail.com>
2014-04-17 22:05:24 +0530

    replace the module pynotify with dbus

--- yahoo_notification_app/yahoomail.py
+++ yahoo_notification_app/yahoomail.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import pynotify
 import sys
 import os
+import dbus
 
 a = os.path.join(sys.path[0], 'logo.jpg')
 
@@ -21,10 +22,19 @@ n = soup.em.b.string
 n = str(n)
 n = n.split(')')[0].lstrip('(')
 n = int(n)
-title = "Notification"
-text  = "You have %d unread mail in your yahoo account" % n
-icon  = a
-pynotify.init("Test Application")
-notification = pynotify.Notification(title, text, icon)
-notification.set_urgency(pynotify.URGENCY_NORMAL)
-notification.show()
+item              = "org.freedesktop.Notifications"
+path              = "/org/freedesktop/Notifications"
+interface         = "org.freedesktop.Notifications"
+app_name          = "Test Application"
+id_num_to_replace = 0
+icon              = a
+title             = "Yahoo Mail"
+text              = "You have %d unread mail in your yahoo account" % n
+actions_list      = ''
+hint              = ''
+time              = 5000   # Use seconds x 1000
+
+bus = dbus.SessionBus()
+notif = bus.get_object(item, path)
+notify = dbus.Interface(notif, interface)
+notify.Notify(app_name, id_num_to_replace, icon, title, text, actions_list, hint, time)
